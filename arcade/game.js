c757ee5fe6d6bcfd23c8b3b8dc7f4527fa20fbfc
mizandarii <132040885+mizandarii@users.noreply.github.com>
2024-09-19 15:18:04 +0300

    Update game.js

--- arcade/game.js
+++ arcade/game.js
@@ -1,5 +1,7 @@
 let game = {
     ctx: null,
+    platform: null,
+    ball: null,
     sprites:{
         background: null,
         ball: null,
@@ -22,20 +24,37 @@ let game = {
         });
     }
 },
-        run(){
+    run(){
         window.requestAnimationFrame(() =>{
             this.render
         });
         },
-        render(){
+    render(){
         this.ctx.drawImage(this.sprites.background, 0, 0);
-        this.ctx.drawImage(this.sprites.ball, 0, 0);
-        this.ctx.drawImage(this.sprites.platform, 0, 0);
+        this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+        this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
         },
         start:function(){
-        this.init();
-        this.preload(() =>{
-            this.run();
-        });
+            this.init();
+            this.preload(() =>{
+                this.run();
+            });
         }
         };
+
+        game.ball ={
+            x: 320,
+            y: 280,
+            width: 20,
+            height: 20
+        };
+
+        
+        game.platform ={
+            x: 280,
+            y: 300,
+        };
+
+        window.addEventListener("load", () => {
+            game.start();
+        });
