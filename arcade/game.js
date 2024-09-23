98c881ab244572a7dd1768323c1e99f5fb1b09e0
mizandarii <132040885+mizandarii@users.noreply.github.com>
2024-09-23 20:52:39 +0300

    Update game.js

--- arcade/game.js
+++ arcade/game.js
@@ -6,6 +6,7 @@ const KEYS = {
 
 let game = {
   ctx: null,
+  running: true,
   platform: null,
   ball: null,
   blocks: [],
@@ -60,6 +61,9 @@ let game = {
       for (let row = 0; row < this.rows; row++) {
           for (let col = 0; col < this.cols; col++) {
               this.blocks.push({
+                  active: true,
+                  width: 60,
+                  height: 20,
                   x: 64 * col + 65,
                   y: 24 * row + 35
               });
@@ -68,29 +72,51 @@ let game = {
   },
 
   update: function() {
+      this.collideBlocks();
+      this.collidePlatform();
+      this.ball.collideWorldBounds();
+      this.platform.collideWorldBounds();
       this.platform.move();
       this.ball.move();
   },
 
+  collideBlocks: function() {
+      for (let block of this.blocks) {
+          if (block.active && this.ball.collide(block)) {
+              this.ball.bumpBlock(block);
+          }
+      }
+  },
+
+  collidePlatform: function() {
+      if (this.ball.collide(this.platform)) {
+          this.ball.bumpPlatform(this.platform);
+      }
+  },
+
   run: function() {
-      window.requestAnimationFrame(() => {
-          this.update();
-          this.render();
-          this.run();
-      });
+      if (this.running) {
+          window.requestAnimationFrame(() => {
+              this.update();
+              this.render();
+              this.run();
+          });
+      }
   },
 
   render: function() {
       this.ctx.clearRect(0, 0, this.width, this.height);
       this.ctx.drawImage(this.sprites.background, 0, 0);
-      this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+      this.ctx.drawImage(this.sprites.ball, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
       this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
       this.renderBlocks();
   },
 
   renderBlocks: function() {
       for (let block of this.blocks) {
-          this.ctx.drawImage(this.sprites.block, block.x, block.y);
+          if (block.active) {
+              this.ctx.drawImage(this.sprites.block, block.x, block.y);
+          }
       }
   },
 
@@ -119,7 +145,7 @@ game.ball = {
 
   start: function() {
       this.dy = -this.velocity;
-      this.dx = game.random(-this.velocity, this.velocity); // Random horizontal direction
+      this.dx = game.random(-this.velocity, this.velocity);
   },
 
   move: function() {
@@ -128,6 +154,68 @@ game.ball = {
       }
       if (this.dx) {
           this.x += this.dx;
+      }
+  },
+
+  collide: function(element) {
+      let x = this.x + this.dx;
+      let y = this.y + this.dy;
+
+      if (x + this.width > element.x &&
+          x < element.x + element.width &&
+          y + this.height > element.y &&
+          y < element.y + element.height) {
+          return true;
+      }
+      return false;
+  },
+
+  collideWorldBounds: function() {
+      let x = this.x + this.dx;
+      let y = this.y + this.dy;
+
+      let ballLeft = x;
+      let ballRight = ballLeft + this.width;
+      let ballTop = y;
+      let ballBottom = ballTop + this.height;
+
+      let worldLeft = 0;
+      let worldRight = game.width;
+      let worldTop = 0;
+      let worldBottom = game.height;
+
+      if (ballLeft < worldLeft) {
+          this.x = 0;
+          this.dx = this.velocity;
+      } else if (ballRight > worldRight) {
+          this.x = worldRight - this.width;
+          this.dx = -this.velocity;
+      }
+
+      if (ballTop < worldTop) {
+          this.y = 0;
+          this.dy = this.velocity;
+      } else if (ballBottom > worldBottom) {
+          game.running = false;
+          alert("Game Over");
+          window.location.reload();
+      }
+  },
+
+  bumpBlock: function(block) {
+      this.dy *= -1;
+      block.active = false;
+  },
+
+  bumpPlatform: function(platform) {
+      if (platform.dx) {
+          this.x += platform.dx;
+      }
+
+      if (this.dy > 0) {
+          this.dy = -this.velocity;
+          let touchX = this.x + this.width / 2;
+          this.dx = this.velocity * platform.getTouchOffset(touchX);
       }
   }
 };
@@ -138,12 +226,14 @@ game.platform = {
   dx: 0,
   x: 280,
   y: 300,
+  width: 100,
+  height: 14,
   ball: game.ball,
 
   fire: function() {
       if (this.ball) {
           this.ball.start();
-          this.ball = null; // Detach ball from platform after firing
+          this.ball = null;
       }
   },
 
@@ -162,15 +252,32 @@ game.platform = {
   move: function() {
       if (this.dx) {
           this.x += this.dx;
-          // Move the ball along with the platform before firing
           if (this.ball) {
               this.ball.x += this.dx;
           }
       }
+  },
+
+  getTouchOffset: function(x) {
+      let diff = (this.x + this.width) - x;
+      let offset = this.width - diff;
+      let result = 2 * offset / this.width;
+      return result - 1;
+  },
+
+  collideWorldBounds: function() {
+      let x = this.x + this.dx;
+      let platformLeft = x;
+      let platformRight = platformLeft + this.width;
+
+      let worldLeft = 0;
+      let worldRight = game.width;
+
+      if (platformLeft < worldLeft || platformRight > worldRight) {
+          this.dx = 0;
+      }
   }
 };
-
-// Start the game when the window loads
 window.addEventListener("load", () => {
   game.start();
 });
